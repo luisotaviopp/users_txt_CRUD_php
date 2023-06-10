00a34d4d68ce0942e5103b1d0f705c66a827290c
Luis Zimermann <luisaslan@gmail.com>
2023-06-10 17:33:16 -0300

    Update crud.php

--- crud.php
+++ crud.php
@@ -1,29 +1,31 @@
 <?php
+
 // Function to write user data to a text file
-function writeUserData($userData) {
-    $mode = file_exists('user_data.txt') ? FILE_APPEND : 0;
+function writeUserData(array $userData): void {
     $data = '';
     foreach ($userData as $user) {
-        $data .= $user['userId'] . ',' . $user['latitude'] . ',' . $user['longitude'] . "\n";
+        $data .= $user['id'] . ',' . $user['timestamp'] . ',' . $user['latitude'] . ',' . $user['longitude'] . PHP_EOL;
     }
-    file_put_contents('user_data.txt', $data, $mode);
+    file_put_contents('user_locations.txt', $data);
 }
 
 // Function to read user data from the text file
-function readUserData() {
-    $userData = array();
-    if (file_exists('user_data.txt')) {
-        $file = fopen('user_data.txt', 'r');
+function readUserData(): array {
+    $userData = [];
+    if (file_exists('user_locations.txt')) {
+        $file = fopen('user_locations.txt', 'r');
         while (($line = fgets($file)) !== false) {
             $data = explode(',', $line);
-            $userId = $data[0];
-            $latitude = $data[1];
-            $longitude = $data[2];
-            $userData[] = array(
-                'userId' => $userId,
+            $id = $data[0];
+            $timestamp = $data[1];
+            $latitude = $data[2];
+            $longitude = $data[3];
+            $userData[$id] = [
+                'id' => $id,
+                'timestamp' => $timestamp,
                 'latitude' => $latitude,
                 'longitude' => $longitude
-            );
+            ];
         }
         fclose($file);
     }
@@ -31,78 +33,166 @@ function readUserData() {
 }
 
 // Function to export user data to JSON
-function exportToJson($userData) {
-    $json = json_encode($userData, JSON_PRETTY_PRINT);
-    file_put_contents('user_data.json', $json);
+function exportToJson(array $userData): void {
+    $json = json_encode(array_values($userData), JSON_PRETTY_PRINT);
+    file_put_contents('user_locations.json', $json);
 }
 
 // Function to list all users
-function listAllUsers($userData) {
+function listAllUsers(array $userData): void {
     foreach ($userData as $user) {
-        echo "User ID: " . $user['userId'] . "\n";
-        echo "Latitude: " . $user['latitude'] . "\n";
-        echo "Longitude: " . $user['longitude'] . "\n\n";
+        echo "User ID: " . $user['id'] . PHP_EOL;
+        echo "Timestamp: " . $user['timestamp'] . PHP_EOL;
+        echo "Latitude: " . $user['latitude'] . PHP_EOL;
+        echo "Longitude: " . $user['longitude'] . PHP_EOL . PHP_EOL;
     }
 }
 
 // Function to update user data
-function updateUserData($userId, $latitude, $longitude) {
+function updateUserData(int $id, float $latitude, float $longitude): void {
     $userData = readUserData();
-    foreach ($userData as &$user) {
-        if ($user['userId'] == $userId) {
-            $user['latitude'] = $latitude;
-            $user['longitude'] = $longitude;
-        }
+    if (isset($userData[$id])) {
+        unset($userData[$id]);
     }
+    $timestamp = date('d-m-Y H:i:s');
+    $userData[$id] = [
+        'id' => $id,
+        'timestamp' => $timestamp,
+        'latitude' => $latitude,
+        'longitude' => $longitude
+    ];
+    writeUserData($userData);
+}
+
+// Function to append or create user data
+function appendOrCreateUserData(int $id, float $latitude, float $longitude): void {
+    $userData = readUserData();
+    if (isset($userData[$id])) {
+        unset($userData[$id]);
+    }
+    $timestamp = date('d-m-Y H:i:s');
+    $userData[$id] = [
+        'id' => $id,
+        'timestamp' => $timestamp,
+        'latitude' => $latitude,
+        'longitude' => $longitude
+    ];
     writeUserData($userData);
 }
 
 // Function to delete user data
-function deleteUserData($userId) {
+function deleteUserData(int $id): void {
     $userData = readUserData();
-    $filteredData = array_filter($userData, function ($user) use ($userId) {
-        return $user['userId'] != $userId;
-    });
-    writeUserData($filteredData);
+    if (isset($userData[$id])) {
+        unset($userData[$id]);
+        writeUserData($userData);
+        echo "User data deleted successfully." . PHP_EOL;
+    } else {
+        echo "User not found." . PHP_EOL;
+    }
 }
 
-// Function to filter user data
-function filterUserData($latitude) {
+// Function to filter user data by ID
+function filterUserData(int $id): array {
     $userData = readUserData();
-    $filteredData = array_filter($userData, function ($user) use ($latitude) {
-        return $user['latitude'] == $latitude;
-    });
+    $filteredData = array_filter($userData, fn($user) => $user['id'] == $id);
     return $filteredData;
 }
 
-// Example usage:
-$users = array(
-    array('userId' => 1, 'latitude' => 40.7128, 'longitude' => -74.0060),
-    array('userId' => 2, 'latitude' => 34.0522, 'longitude' => -118.2437),
-    array('userId' => 3, 'latitude' => 51.5074, 'longitude' => -0.1278)
-);
+// Haversine formula to calculate distance between two points
+function calculateDistance(float $lat1, float $lon1, float $lat2, float $lon2): float {
+    $earthRadius = 6371; // Earth's radius in kilometers
 
-writeUserData($users); // Write user data to the file
+    $lat1Rad = deg2rad($lat1);
+    $lon1Rad = deg2rad($lon1);
+    $lat2Rad = deg2rad($lat2);
+    $lon2Rad = deg2rad($lon2);
 
-$additionalUsers = array(
-    array('userId' => 4, 'latitude' => 35.6895, 'longitude' => 139.6917),
-    array('userId' => 5, 'latitude' => 48.8566, 'longitude' => 2.3522)
-);
+    $deltaLat = $lat2Rad - $lat1Rad;
+    $deltaLon = $lon2Rad - $lon1Rad;
 
-writeUserData($additionalUsers); // Append additional user data to the file
+    $a = sin($deltaLat / 2) * sin($deltaLat / 2) +
+        cos($lat1Rad) * cos($lat2Rad) *
+        sin($deltaLon / 2) * sin($deltaLon / 2);
 
-$allUsers = readUserData(); // Read user data from the file
+    $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
 
-listAllUsers($allUsers); // List all users
+    $distance = $earthRadius * $c;
+    return $distance;
+}
 
-exportToJson($allUsers); // Export user data to JSON
+// Function to list users sorted by distance to the reference latitude and longitude
+function listUsersByDistance(array $userData, float $referenceLatitude, float $referenceLongitude): void {
+    $distances = [];
+    foreach ($userData as $user) {
+        $distance = calculateDistance($referenceLatitude, $referenceLongitude, $user['latitude'], $user['longitude']);
+        $distances[$user['id']] = $distance;
+    }
+
+    asort($distances);
+
+    foreach ($distances as $id => $distance) {
+        $user = $userData[$id];
+        echo "User ID: " . $user['id'] . PHP_EOL;
+        echo "Distance: " . $distance . " km" . PHP_EOL;
+        echo "Timestamp: " . $user['timestamp'] . PHP_EOL;
+        echo "Latitude: " . $user['latitude'] . PHP_EOL;
+        echo "Longitude: " . $user['longitude'] . PHP_EOL . PHP_EOL;
+    }
+}
+
+// Create an empty user_locations.txt file if it doesn't exist
+if (!file_exists('user_locations.txt')) {
+    file_put_contents('user_locations.txt', '');
+}
+
+// Test the functions
+$referenceLatitude = 40.7128;
+$referenceLongitude = -74.0060;
+
+// Write user data
+writeUserData([
+    [
+        'id' => 1,
+        'timestamp' => date('d-m-Y H:i:s'),
+        'latitude' => 40.7128,
+        'longitude' => -74.0060
+    ],
+    [
+        'id' => 2,
+        'timestamp' => date('d-m-Y H:i:s'),
+        'latitude' => 37.7749,
+        'longitude' => -122.4194
+    ],
+    [
+        'id' => 3,
+        'timestamp' => date('d-m-Y H:i:s'),
+        'latitude' => 51.5074,
+        'longitude' => -0.1278
+    ],
+]);
+
+// Read user data
+$userData = readUserData();
+
+// List all users
+listAllUsers($userData);
+
+// Export user data to JSON
+exportToJson($userData);
 
 // Update user data
-updateUserData(2, 37.7749, -122.4194);
+updateUserData(2, 35.6895, 139.6917);
+
+// Append or create user data
+appendOrCreateUserData(4, 41.8781, -87.6298);
 
 // Delete user data
-deleteUserData(3);
+deleteUserData(1);
 
 // Filter user data
-$filteredUsers = filterUserData(40.7128);
-listAllUsers($filteredUsers);
+$filteredData = filterUserData(3);
+var_dump($filteredData);
+
+// List users by distance
+listUsersByDistance($userData, $referenceLatitude, $referenceLongitude);
